a636d9261fc54dccb50df97b4c0c58cece95a90d
Van Thai <lethai281005@gmail.com>
2025-09-22 23:31:24 +0700

    "SearchBar"

--- src/layouts/LayoutDefault/index.jsx
+++ src/layouts/LayoutDefault/index.jsx
@@ -1,36 +1,48 @@
 import React from "react";
 import { Layout } from "antd";
+
 import SiteFooter from "../../components/Footer";
-import { Outlet, Link } from 'react-router-dom'
-import DefaultHeader from '../../components/Header/DefaultHeader';
-const {Content} = Layout;
+import { Outlet, Link } from "react-router-dom";
+import DefaultHeader from "../../components/Header/DefaultHeader";
+const { Content } = Layout;
 
 const LayoutDefault = () => {
-    return (
-        <>
-        <Layout>
-
-            <DefaultHeader/>
-
-      <Content style={{ padding: "20px", background: "#f0f2f5" }}>
-        <div
+  return (
+    <>
+      <Layout>
+        <Header
           style={{
             background: "#fff",
-            minHeight: "280px",
-            padding: "24px",
-            borderRadius: "8px",
-            boxShadow: "0 1px 3px rgba(0,0,0,0.1)",
+            padding: 0,
+            height: "auto",
+            lineHeight: "normal",
+            gap: "64px",
           }}
         >
-          <Outlet />
-        </div>
-      </Content>
+          <DefaultHeader />
+          <SearchBar />
+        </Header>
 
-      <SiteFooter />
-    </Layout>
+        <DefaultHeader />
+
+        <Content style={{ padding: "20px", background: "#f0f2f5" }}>
+          <div
+            style={{
+              background: "#fff",
+              minHeight: "280px",
+              padding: "24px",
+              borderRadius: "8px",
+              boxShadow: "0 1px 3px rgba(0,0,0,0.1)",
+            }}
+          >
+            <Outlet />
+          </div>
+        </Content>
+
+        <SiteFooter />
+      </Layout>
     </>
   );
 };
-
 
 export default LayoutDefault;
