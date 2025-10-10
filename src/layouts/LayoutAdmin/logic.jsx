4e9bb2cfaab92625e7affe6baca6dfec5ea9a13a
phuc220204 <phuc220204@gmail.com>
2025-10-11 03:22:49 +0700

    Cập nhập Trang Admin sau merge

--- src/layouts/LayoutAdmin/logic.jsx
+++ src/layouts/LayoutAdmin/logic.jsx
@@ -1,4 +1,13 @@
-import { DashboardOutlined, TeamOutlined } from "@ant-design/icons";
+import {
+  DashboardOutlined,
+  TeamOutlined,
+  AppstoreOutlined,
+  CarOutlined,
+  ThunderboltOutlined,
+  TagsOutlined,
+  CrownOutlined,
+  SettingOutlined,
+} from "@ant-design/icons";
 import { useSidebar } from "@hooks/useSidebar";
 
 export function useLayoutAdmin() {
@@ -15,6 +24,43 @@ export function useLayoutAdmin() {
       icon: <TeamOutlined />,
       label: "Quản lý tài khoản",
       path: "/admin/accounts",
+    },
+    {
+      key: "product-management",
+      icon: <AppstoreOutlined />,
+      label: "Quản lý sản phẩm",
+      children: [
+        {
+          key: "category",
+          icon: <TagsOutlined />,
+          label: "Danh mục",
+          path: "/admin/product/category",
+        },
+        {
+          key: "brand",
+          icon: <CrownOutlined />,
+          label: "Thương hiệu",
+          path: "/admin/product/brand",
+        },
+        {
+          key: "model",
+          icon: <SettingOutlined />,
+          label: "Mẫu xe",
+          path: "/admin/product/model",
+        },
+        {
+          key: "vehicle",
+          icon: <CarOutlined />,
+          label: "Xe điện",
+          path: "/admin/product/vehicle",
+        },
+        {
+          key: "battery",
+          icon: <ThunderboltOutlined />,
+          label: "Pin xe điện",
+          path: "/admin/product/battery",
+        },
+      ],
     },
   ];
 
