9697e42e170fc4cadfb0d5110f37a4991af0e9e2
LE THANH TRONG <lethanhtrong161005@gmail.com>
2025-10-14 12:38:25 +0700

    merge main

--- src/components/Action/HeaderAction.jsx
+++ src/components/Action/HeaderAction.jsx
@@ -27,16 +27,6 @@ const HeaderAction = () => {
     handleLoginSubmit,
   } = auth;
 
-  // // ⬇️ click “Quản lý tin”
-  // const handleClickManageListing = () => {
-  //   if (isLoggedIn) {
-  //     navigate(MANAGE_LISTINGS_PATH);
-  //   } else {
-  //     auth.setRedirectAfterLogin(MANAGE_LISTINGS_PATH);
-  //     messageApi.info("Vui lòng đăng nhập để quản lý tin");
-  //     setOpenLogin(true);
-  //   }
-  // };
 
   const displayName = user?.fullName || user?.name || user?.sub || "Hồ sơ";
   const menuItems = getMenuItems();
@@ -62,7 +52,7 @@ const HeaderAction = () => {
             <Button
               onClick={() =>
                 handleLoginRequire(
-                  "/my-ads",
+                  MANAGE_LISTINGS_PATH,
                   "Vui lòng đăng nhập để quản lý tin"
                 )
               }
